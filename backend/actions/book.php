94c0254f0f2d129784c2ce575ec84589299d0195
Abdulmanan <mohammadabdulmanan24@gmail.com>
2025-05-18 22:36:30 +0800

    update actions book

--- backend/actions/book.php
+++ backend/actions/book.php
@@ -2,12 +2,4 @@
 include_once '../../config/init.php';
 
 $booksController->display();
-
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $title = $_POST['title'];
-    $author = $_POST['author'];
-    $copies = $_POST['copies'];
-    $booksController->add($title, $author, $copies);
-}
-
 ?>
